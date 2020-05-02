f903f493643b3d8190e88f94456c8942d69817c0
jonathanprozzi <jonathanprozzi@gmail.com>
2020-05-02 01:18:58 -0400

    started tweaking theme config in doczrc.js - the header button bg color is nested insidecolor - mode - (light/dark) - header - button - bg

--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,21 @@ export default {
   title: "Project Waves Setup",
   themeConfig: {
     initialColorMode: "dark",
+    colors: {
+      modes: {
+        light: {
+          header: {
+            button: {},
+          },
+        },
+        dark: {
+          header: {
+            button: {
+              // bg: "#7ba7ff",
+            },
+          },
+        },
+      },
+    },
   },
 }
